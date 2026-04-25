227109bb08bdb80ee4a7c593528216eaee237263
0zannM <musaogullariozan@gmail.com>
2026-04-25 14:46:37 +0300

    Reduce temperature in generationConfig to 0.5

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ async function askGemini(userMessage, recentMessages = [], senderPlayerId = null
   
   const apiConfig = {
     tools: TOOLS,
-    generationConfig: { maxOutputTokens: 2000, temperature: 0.7 }
+    generationConfig: { maxOutputTokens: 2000, temperature: 0.5 }
   };
 
   let res;
